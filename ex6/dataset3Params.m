1d23ab93f7150499ae4f9c4e66e185079d537335
Junstar92 <pord7457@gmail.com>
2020-08-25 02:21:08 +0900

    Update ex6 codes

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_val = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_val = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+for i = 1:length(C_val)
+  for j = 1:length(sigma_val)
+    model = svmTrain(X, y, C_val(i), @(x1, x2) gaussianKernel(x1, x2, sigma_val(j)));
+    prediction = svmPredict(model, Xval);
+    Error(i, j) = mean(double(prediction ~= yval));
+  endfor
+endfor
 
-
-
-
+min_Error = min(min(Error));
+[i j] = find(Error == min_Error);
+C = C_val(i);
+sigma = sigma_val(j);
 
 % =========================================================================
 
